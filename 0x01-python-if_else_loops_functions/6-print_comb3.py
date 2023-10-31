734eb30719747c94031e967b814f84130c5d5899
mostafazhrn <mostafazhrn@gmail.com>
2023-10-31 15:36:51 -0400

    fixedPyStyle6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for x in range(0, 10):
         if x >= y:
             continue
         elif x == 8 and y == 9:
-             print("{:d}{:d}".format(x, y))
+            print("{:d}{:d}".format(x, y))
         else:
             print("{:d}{:d}, ".format(x, y), end="")
