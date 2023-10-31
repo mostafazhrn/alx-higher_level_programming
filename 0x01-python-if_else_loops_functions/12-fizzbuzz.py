d610bcf31fde6907a959dd783a0e840d21fd5b7a
mostafazhrn <mostafazhrn@gmail.com>
2023-10-31 16:26:28 -0400

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz ", end=" ")
+            continue
+        elif x % 3 == 0:
+            print("Fizz ", end=" ")
+            continue
+        elif x % 5 == 0:
+            print("Buzz ", end=" ")
+            continue
+        else:
+            print("{:d}".format(x), end=" ")
