915dd76ea8865e291bdbffa9d54ff2f3b6f009f5
mostafazhrn <mostafazhrn@gmail.com>
2023-11-02 10:28:58 -0400

    argsSys2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     arg_cnt = len(sys.argv) - 1
     if arg_cnt == 0:
-        print("0 argmuments")
+        print("0 argmuments.")
     elif arg_cnt == 1:
         print("1 argument:")
     else:
