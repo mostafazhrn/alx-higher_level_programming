5495d323ed5de616b9cf8416546e73ea77f7849a
mostafazhrn <mostafazhrn@gmail.com>
2023-11-08 16:10:27 -0500

    BestScoreFix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     if a_dictionary:
-          val = max(a_dictionary.values())
-          for x, y in a_dictionary.items():
-               if y == val:
-                    return (x)
-     else:
-          return (None)
+    if a_dictionary:
+        val = max(a_dictionary.values())
+        for x, y in a_dictionary.items():
+            if y == val:
+                return (x)
+    else:
+        return (None)
