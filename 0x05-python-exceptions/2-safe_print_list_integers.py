c77d86cdf2639f297dbe95f50e1ae738ec5261d4
mostafazhrn <mostafazhrn@gmail.com>
2023-11-20 13:01:50 -0500

    indexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     compte = 0
     y = 0
-    while  y < x:
+    while y < x:
         try:
             print("{:d}".format(my_list[y]), end="")
             compte += 1
