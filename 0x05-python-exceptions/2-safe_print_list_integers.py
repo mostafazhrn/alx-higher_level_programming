4f448c1d45070e66555f6395087a8d24b16fbbbb
mostafazhrn <mostafazhrn@gmail.com>
2023-11-20 13:00:03 -0500

    indexError

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    compte = 0
+    y = 0
+    while  y < x:
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            compte += 1
+        except TypeError:
+            None
+        except ValueError:
+            None
+        y += 1
+    print("")
+    return compte
