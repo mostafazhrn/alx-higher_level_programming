103ef6f2d8e62173866bc81880603728286cd115
mostafazhrn <mostafazhrn@gmail.com>
2023-11-20 16:21:21 -0500

    ERRint

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -7,4 +9,3 @@ def safe_print_integer_err(value):
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return False
-    
