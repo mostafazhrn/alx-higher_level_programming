77f4ff282064def9ae90fa8656be610095360c22
mostafazhrn <mostafazhrn@gmail.com>
2023-11-21 08:49:21 -0500

    __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this code create a class with private attribute size"""
+
+
+class Square:
+
+    """this start the priv attribuate size"""
+    def __init__(self, size):
+        """ this start the __size"""
+        self.__size = size
