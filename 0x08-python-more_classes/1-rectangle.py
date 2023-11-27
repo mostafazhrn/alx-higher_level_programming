c7aa1ee63b1930e5fb00e0794a4602aa72293d9c
mostafazhrn <mostafazhrn@gmail.com>
2023-11-27 05:04:15 -0500

    width&heightSet

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,22 +15,6 @@ class Rectangle:
         self.__width = width
 
     @property
-    def width(self):
-        """This is the def width(self) instance: to retrieve width"""
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """def width(self, value): to set it"""
-
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >=0")
-        self.__width = value
-
-    @property
     def height(self):
         """This is the def height(self): to retrieve height"""
 
@@ -45,3 +29,19 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >=0")
         self.__height = value
+
+    @property
+    def width(self):
+        """This is the def width(self) instance: to retrieve width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """def width(self, value): to set it"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
