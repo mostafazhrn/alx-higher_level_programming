200171e3bcabe012e6c71e633d48f99dcff78f28
mostafazhrn <mostafazhrn@gmail.com>
2023-11-27 05:01:15 -0500

    width&heightSet

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,33 +2,44 @@
 """This module defines a rectangle
 based on the previous script
 """
+
+
 class Rectangle:
     """This class have private instance.
     it shall take two argument width& height.
     """
     def __init__(self, width=0, height=0):
         """Starting instance of optional widthand height"""
-        self.__width = width
+
         self.__height = height
+        self.__width = width
+
     @property
     def width(self):
-        """def width(self): to retrieve it"""
+        """This is the def width(self) instance: to retrieve width"""
+
         return self.__width
+
     @width.setter
     def width(self, value):
         """def width(self, value): to set it"""
+
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >=0")
         self.__width = value
+
     @property
     def height(self):
-        """def height(self): to retrieve it"""
+        """This is the def height(self): to retrieve height"""
+
         return self.__height
+
     @height.setter
     def height(self, value):
         """This set the height of the rectangle"""
+
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
