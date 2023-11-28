ea70244fd12ca6bb51e9f80a68b7a31a6c1b5c59
mostafazhrn <mostafazhrn@gmail.com>
2023-11-28 10:38:35 -0500

    text_identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module prints a text with 2 new.
+Ln after each character as follows: ., ? and :
+Raises:
+TypeError: if text is not a string
+"""
+
+
+def text_indentation(text):
+    """Print txt with 2 new lines chars.
+    Args are text (str) which is text to print
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    x = 0
+    while x < len(text) and text[x] == ' ':
+        x += 1
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] == "\n" or text[x] in ".?:":
+            if text[x] in ".?:":
+                print("\n")
+                x += 1
+                while x < len(text) and text[x] == '':
+                    x += 1
+                    continue
+        x += 1
