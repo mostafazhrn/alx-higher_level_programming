f29c0f6c7d2e8040abd2029907c486bd6a2e2b89
mostafazhrn <mostafazhrn@gmail.com>
2023-11-28 10:53:12 -0500

    text_identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,15 +14,11 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     x = 0
-    while x < len(text) and text[x] == ' ':
-        x += 1
-    while x < len(text):
+    for x in range(x, len(text)):
         print(text[x], end="")
         if text[x] == "\n" or text[x] in ".?:":
             if text[x] in ".?:":
                 print("\n")
+            x += 1
+            while x < len(text) and text[x] == '':
                 x += 1
-                while x < len(text) and text[x] == '':
-                    x += 1
-                    continue
-        x += 1
