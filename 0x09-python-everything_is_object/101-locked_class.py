bfda61515813664ba8b4c7ed34d10cede06194ff
mostafazhrn <mostafazhrn@gmail.com>
2023-11-28 07:29:05 -0500

    advanced1

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is a locked class it shall prevent the user
+From creating new instance unless called frstname
+"""
+
+
+class LockedClass:
+    """This is the locked cls"""
+    __slots__ = ["first_name"]
