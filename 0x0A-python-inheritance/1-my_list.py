121fc6867fb6a0046ea7684b8ad8b9fe40f11f32
mostafazhrn <mostafazhrn@gmail.com>
2023-12-04 12:46:34 -0500

    my_list&test

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module takes my list from list"""
+
+
+class MyList(list):
+    """
+    This class for method of sorted list
+    """
+    pass
+
+    def print_sorted(self):
+        """ This function print sorted list"""
+
+        print(sorted(self))
