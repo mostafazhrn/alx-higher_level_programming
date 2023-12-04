dd10db414b885f970b650aa3b4888cab1089c468
mostafazhrn <mostafazhrn@gmail.com>
2023-12-04 12:54:12 -0500

    is_same

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module return true if object is the instance specified
+"""
+
+
+def is_same_class(obj, a_class):
+    """This function shall return true if same as class"""
+
+    return (type(obj) == a_class)
