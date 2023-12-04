40e3637eedcb6f420b8386347a4ea40b853fdbd5
mostafazhrn <mostafazhrn@gmail.com>
2023-12-04 14:26:47 -0500

    areaGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module is class baseGeomtry"""
+
+
+class BaseGeometry:
+    """This class is baseGeometry"""
+
+    def area(self):
+        """This func raise instance area not iplemented"""
+
+        raise Exception("area() is not implemented")
