2b56376145586f0bf828c357051d5eb68dd44a14
mostafazhrn <mostafazhrn@gmail.com>
2023-12-05 10:49:51 -0500

    class2Json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module shall return dic discription with data struct"""
+
+
+def class_to_json(obj):
+    """This func return dic descrip. with simple data struct"""
+
+    return obj.__dict__
