8248d4ac925099e2f93cf520297a4b4a43d87ce9
mostafazhrn <mostafazhrn@gmail.com>
2023-12-05 08:48:21 -0500

    toJson

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module return JSON of obj"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """This funcreturn json rep of an obj"""
+
+    return json.dumps(my_obj)
