b4c78873a6235239531fb10c7a528e880bedf409
mostafazhrn <mostafazhrn@gmail.com>
2023-12-05 06:57:00 -0500

    writeFile

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/ust/bin/python3
+"""This module writes str to text and return num of chars"""
+
+
+def write_file(filename="", text=""):
+    """This writes str to text and return num of chars"""
+
+    with open(filename, mode="w", encoding="utf-8") as x:
+        return x.write(text)
