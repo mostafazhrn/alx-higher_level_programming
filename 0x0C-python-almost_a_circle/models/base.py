109e6467cfa0d7ee150f200d8d02c35bf8deb03f
mostafazhrn <mostafazhrn@gmail.com>
2023-12-09 08:59:43 -0500

    base&sq

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,68 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """This rep to json method"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """This rep the save to file method
+        Args:
+           cls: represent the class
+           list_objs: rep the list objects
+        Returns:
+           None
+        """
+        filename = cls.__name__ + ".json"
+        nuevo_lst = []
+        if list_objs is not None:
+            for x in list_objs:
+                nuevo_lst.append(cls.to_dictionary(x))
+        with open(filename, "w") as y:
+            y.write(cls.to_json_string(nuevo_lst))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """This represent the from json method"""
+        if json_string is None or len(json_string) == 0:
+            return ([])
+        else:
+            return (loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """This shall represent the create method
+        Args:
+            cls: this rep class
+            **dictionary:this rep dict
+        Returns:
+            dummy
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        if cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return (dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        """This shall return a list of inst"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as y:
+                nuevo_lst = cls.from_json_string(y.read())
+                for x in range(len(nuevo_lst)):
+                    nuevo_lst[x] = cls.create(**nuevo_lst[x])
+                return nuevo_lst
+        except FileNotFoundError:
+            return ([])
+        except Exception as e:
+            print("An error occurred while loading from file:", str(e))
+            return ([])
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,47 @@ class Square(Rectangle):
         """This shall rep str method"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x,
                                                   self.y, self.width))
+
+    @property
+    def size(self):
+        """This represent the size getter shall get & retrn size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """This shall represent size setter"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """This represent update method
+        Args:
+           *args: arguments
+           **kwargs: keyword arguments
+        Returns:
+            None
+        """
+        if args is not None and len(args) != 0:
+            for x in range(len(args)):
+                if x == 0:
+                    self.id = args[x]
+                if x == 1:
+                    self.size = args[x]
+                if x == 2:
+                    self.x = args[x]
+                if x == 3:
+                    self.y = args[x]
+        elif kwargs is not None and len(kwargs) != 0:
+            for x in kwargs:
+                if x == "id":
+                    self.id = kwargs[x]
+                if x == "size":
+                    self.size = kwargs[x]
+                if x == "x":
+                    self.x = kwargs[x]
+                if x == "y":
+                    self.y = kwargs[x]
+
+    def to_dictionary(self):
+        """This rep the dict method"""
+        return ({"id": self.id, "size": self.size, "x": self.x, "y": self.y})
