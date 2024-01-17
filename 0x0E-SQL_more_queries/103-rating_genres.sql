146cad57c2ba53429ba8da2bc2c15124bb691e48
mostafazhrn <mostafazhrn@gmail.com>
2024-01-17 17:26:52 -0500

    rateGenre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,5 @@
+-- This script shall list genres in db by rate
+SELECT g.name, SUM(r.rate) AS rating
+FROM (`tv_genres` g JOIN `tv_show_genres` sg ON g.id = sg.genre_id)
+JOIN tv_show_ratings r ON sg.show_id = r.show_id GROUP BY g.name
+ORDER BY rating DESC;
