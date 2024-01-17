2f3b582880ac9739853778e7a60414d35717996a
mostafazhrn <mostafazhrn@gmail.com>
2024-01-17 17:16:01 -0500

    onlyComedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,4 @@
+-- This script shall list all comedy shows in db
+SELECT s.title FROM (tv_genres g JOIN tv_show_genres sg ON g.id = sg.genre_id)
+JOIN tv_shows s ON sg.show_id = s.id WHERE g.name = "Comedy" ORDER BY
+s.title ASC;
