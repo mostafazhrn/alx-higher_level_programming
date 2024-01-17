4d4b5776bdfca848b3310ab4172fc2d53e397a35
mostafazhrn <mostafazhrn@gmail.com>
2024-01-17 16:42:03 -0500

    no_genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- This script shall list all shows without genre link
+SELECT s.title, g.genre_id FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id WHERE g.genre_id IS NULL ORDER BY s.title, g.genre_id;
