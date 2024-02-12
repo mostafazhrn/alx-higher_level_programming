58744c14ea178942471d65d6c2511f2bb37be055
mostafazhrn <mostafazhrn@gmail.com>
2024-02-12 12:28:38 -0500

    printMyNum

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script shall print My NUm + arg
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
