e204f026c3a0f6f1609865c3754f7d6cd0185baf
mostafazhrn <mostafazhrn@gmail.com>
2024-02-12 12:36:03 -0500

    3linesLoop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// This script shall print 3 lines using loop
+const lignes = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lignes) {
+  console.log(line);
+}
