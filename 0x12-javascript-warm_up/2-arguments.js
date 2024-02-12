7862483649de0bd1d6f1ae5ac75ea6ec5f83ed4f
mostafazhrn <mostafazhrn@gmail.com>
2024-02-12 11:58:19 -0500

    argumentsPassed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// This script shall print msg depending on num of args
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
