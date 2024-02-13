eec4e8d4eb4e6d8005766162cfb140b056556d2d
mostafazhrn <mostafazhrn@gmail.com>
2024-02-13 11:27:23 -0500

    2Rec

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// This code shall define class rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
