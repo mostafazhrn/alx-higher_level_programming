84fd2f43558301920c5bc27d153a6d2bad93d991
mostafazhrn <mostafazhrn@gmail.com>
2024-02-15 05:14:35 -0500

    1stCommit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# This code shall list all states from hbtn_usa db
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states")
+    [print(state) for state in cr.fetchall()]
+    cr.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the task 0x0F-python-object_relational_mapping for the alx software engineeing awhich ocnsists of 15 mandatory tasks and 3 advanced as follows
