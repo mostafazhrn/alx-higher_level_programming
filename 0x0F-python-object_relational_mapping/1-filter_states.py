8270f2efce0953bf22d7b37d421f0d3c5f0736fd
mostafazhrn <mostafazhrn@gmail.com>
2024-02-15 13:02:18 -0500

    filterStates

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# This code shall list all states in db name start with n
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in cr.fetchall() if state[1][0] == "N"]
+    cr.close()
