fa1cecc1e52683b6004b87042f0c21c7bc40732b
mostafazhrn <mostafazhrn@gmail.com>
2024-02-16 08:43:45 -0500

    useFOrmat

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,12 +2,14 @@
 """ This script shall display all values of table that match arg"""
 import MySQLdb
 import sys
+from sys import argv
 
 if __name__ == "__main__":
+    """ This shall access the db and get states"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cr = db.cursor()
-    cr.execute("SELECT * FROM states WHERE name = %s ORDER BY id",
-               (sys.argv[4],))
+    cr.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id"
+               .format(sys.argv[4]))
     [print(state) for state in cr.fetchall()]
     cr.close()
