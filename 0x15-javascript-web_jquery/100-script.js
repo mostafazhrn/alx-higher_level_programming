45375994fefb140def6f9ce8421618a66ae463f5
mostafazhrn <mostafazhrn@gmail.com>
2024-04-03 18:37:30 -0400

    noJquery_doc_loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// This script shall update text color to red without jquery
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hdr = document.querySelector('header');
+  hdr.style.color = '#FF0000';
+});
